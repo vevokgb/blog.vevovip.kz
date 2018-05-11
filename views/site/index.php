38278572549e371448efdd1289140f02ea8806a8
Roman Akhmetyanov <vevogm@gmail.com>
2018-05-11 12:33:35 +0600

    Вывод заголовка, текста и даты статьи

--- views/site/index.php
+++ views/site/index.php
@@ -23,30 +23,22 @@ use yii\widgets\LinkPager;
                             <header class="entry-header text-center text-uppercase">
                                 <h6><a href="#"> <?= $article->category->title; ?></a></h6>
 
-                                <h1 class="entry-title"><a href="blog.html">Home is peaceful place</a></h1>
+                                <h1 class="entry-title"><a href="blog.html"><?= $article->title; ?></a></h1>
 
 
                             </header>
                             <div class="entry-content">
-                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                    tevidulabore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-                                    accusam et
-                                    justo duo dolores rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                                    Lorem
-                                    ipsum dolor sit am Lorem ipsum dolor sitconsetetur sadipscing elitr, sed diam nonumy
-                                    eirmod tempor invidunt ut labore et dolore maliquyam erat, sed diam voluptua.
-                                </p>
-
+                                <p><?= $article->description; ?></p>
                                 <div class="btn-continue-reading text-center text-uppercase">
                                     <a href="blog.html" class="more-link">Continue Reading</a>
                                 </div>
                             </div>
                             <div class="social-share">
-                                <span class="social-share-title pull-left text-capitalize">By <a href="#">Rubel</a> On February 12, 2016</span>
+                                <span class="social-share-title pull-left text-capitalize">By <a
+                                            href="#">Rubel</a> On <?= $article->date; ?></span>
                                 <ul class="text-center pull-right">
                                     <li><a class="s-facebook" href="#"><i class="fa fa-eye"></i></a></li>
-                                    325
-                                </ul>
+                                    <?= (int)$article->viewed; ?>
                             </div>
                         </div>
                     </article>
