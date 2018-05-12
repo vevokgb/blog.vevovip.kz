fe9b1322d3dbbe2a2742a06cc9093ace624a7458
Roman Akhmetyanov <vevogm@gmail.com>
2018-05-12 23:28:19 +0600

    Добавлен метод behaviors для разграничения прав входа в админ панель

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,20 +2,44 @@
 
 namespace app\modules\admin;
 
+use Yii;
+use yii\filters\AccessControl;
+
 /**
  * admin module definition class
  */
 class Module extends \yii\base\Module
 {
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public $layout = '/admin';
+
     public $controllerNamespace = 'app\modules\admin\controllers';
 
     /**
-     * {@inheritdoc}
+     * @return array
      */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'denyCallback' => function ($rule, $action) {
+                    throw new \yii\web\NotFoundHttpException();
+                },
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'matchCallback' => function ($rule, $action) {
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
+    }
+
     public function init()
     {
         parent::init();
